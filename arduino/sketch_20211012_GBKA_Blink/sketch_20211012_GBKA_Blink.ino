78f15af19761924583c0474b1f54da05463b46ae
hohno-46466 <hohno.46466@gmail.com>
2021-10-18 06:30:05 +0900

    updated by @hohno_at_kuimc on penguin

--- arduino/sketch_20211012_GBKA_Blink/sketch_20211012_GBKA_Blink.ino
+++ arduino/sketch_20211012_GBKA_Blink/sketch_20211012_GBKA_Blink.ino
@@ -43,9 +43,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(100);                       // wait for a second
 }
 
--- examples/README.md
+++ examples/README.md
@@ -1,1 +1,16 @@
 # eZone--mgmg--UCI2021/examples
+
+    examples
+     ├── 0-1_update_your_apt.txt
+     ├── 0-2_install_additional_commands.txt
+     ├── 1-1_MQTT1.txt
+     ├── 1-2_MQTT2.txt
+     ├── 2-1_kotoriotoko-1.txt
+     ├── 2-2_kotoriotoko-2.txt
+     ├── 2-x_kotoriotoko-3.txt
+     ├── 3-1_GBKA-1.txt
+     ├── README.md
+     └── template.txt
+     
+     0 directories, 10 files (Mon Oct 18 06:25:38 JST 2021)
+
